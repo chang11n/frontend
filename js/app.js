d275860f62d7262ed8e9cf76da700b83dd1544f7
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2015-09-05 20:12:38 -0400

    Remove deprecated syntax Signed-off-by:Rugal Bernstein <rugal.bernstein.0@gmail.com>

--- js/app.js
+++ js/app.js
@@ -2,6 +2,12 @@
 
 (function () {
     var PTapp = angular.module('PTapp', []);
+    PTapp.config(['$httpProvider', function ($httpProvider) {
+            $httpProvider.defaults.headers.common['Accept'] = 'application/json';
+            $httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
+            delete $httpProvider.defaults.headers.common['X-Requested-With'];
+            $httpProvider.defaults.headers.common['Accept-Language'] = 'en, en-US';
+        }]);
 
 
     PTapp.controller('userController', ['$http', function ($http) {
