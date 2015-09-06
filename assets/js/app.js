f985d50e8d3744a7c38262ccf6b87887832cdc9a
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2015-09-06 15:25:55 -0400

    Add popup window Signed-off-by:Rugal Bernstein <rugal.bernstein.0@gmail.com>

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,29 +1,58 @@
 /* global angular */
-
-(function () {
-    var PTapp = angular.module('PTapp', []);
-    PTapp.config(['$httpProvider', function ($httpProvider) {
-            $httpProvider.defaults.headers.common['Accept'] = 'application/json';
-            $httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
-            delete $httpProvider.defaults.headers.common['X-Requested-With'];
-            $httpProvider.defaults.headers.common['Accept-Language'] = 'en, en-US';
-        }]);
+var PTapp = angular.module('PTapp', ['ui.bootstrap']);
+PTapp.config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.defaults.headers.common['Accept'] = 'application/json';
+        $httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+        $httpProvider.defaults.headers.common['Accept-Language'] = 'en, en-US';
+    }]);
 
 
-    PTapp.controller('userController', ['$http', function ($http) {
-            var rest = this;
-            rest.user = [];
 
-            this.send = function (user) {
-                $http({method: 'POST', url: 'http://localhost:8080/signin',
-                    headers: {
-                        "id": user.email,
-                        "credential": user.password
-                    }})
-                        .then(function (response) {
-                            rest.user = response.data;
-                        }, function (response) {
-                            alert('Failed: ' + response.status);
-                        });};
-        }]);
-})();
+// Please note that $modalInstance represents a modal window (instance) dependency.
+// It is not the same as the $modal service used above.
+var WindowInstanceController = PTapp.controller('WindowInstanceController', function ($scope, $modalInstance, content) {
+    $scope.content = content;
+    $scope.ok = function () {
+        $modalInstance.close();
+    };
+});
+
+
+var signinController = PTapp.controller('SigninController', ['$http', '$scope', '$modal', '$log', function ($http, $scope, $modal, $log) {
+        $scope.open = function (json) {
+            var modalInstance = $modal.open({
+                animation: true,
+                templateUrl: 'assets/template/SigninFailed.html',
+                controller: 'WindowInstanceController',
+                size: 'sm',
+                resolve: {
+                    content: function () {
+                        return json.message;
+                    }
+                }
+            });
+            modalInstance.result.then(function () {
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+
+        var rest = this;
+        rest.responsebody = [];
+        this.send = function (user) {
+            $http({method: 'POST', url: 'http://localhost:8080/signin',
+                headers: {
+                    "id": user.email,
+                    "credential": user.password
+                }})
+                    .then(function (response) {
+                        rest.responsebody = response.data;
+                        $scope.open(response.data);
+                    }, function (response) {
+                        $scope.open(response.data);
+                    });
+        };
+
+    }]);
